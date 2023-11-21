2f4429d699dfca554007f3fdd64df6e8ae35841b
smazik02 <s.mazik2002@gmail.com>
2023-11-21 23:30:41 +0100

    21.11.2023

--- server.py
+++ server.py
@@ -16,6 +16,12 @@ voltChart = px.line(x=sim.time,
 powerChart = px.line(x=sim.time,
                      y=sim.heaterPower,
                      title='Heater power')
+inFlowChart = px.line(x=sim.time,
+                      y=sim.inFlow,
+                      title='InFlow')
+eChart = px.line(x=sim.time,
+                 y=sim.e,
+                 title='Uchyb')
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
@@ -33,6 +39,14 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='power',
         figure=powerChart
+    ),
+    dcc.Graph(
+        id='imflow',
+        figure=inFlowChart
+    ),
+    dcc.Graph(
+        id='uchyb',
+        figure=eChart
     )
 ])
 
--- sim.py
+++ sim.py
@@ -12,9 +12,8 @@ ENERGY_MIN = 0
 ENERGY_MAX = 1500
 CV = 4180  # ciepło właściwe wody, J * 1/kg * 1/K
 
-KP = 0.025  # Wzmocnienie regulatora
-TI = 2.5  # Czas zdwojenia
-TD = 3.0  # Czas wyprzedzenia
+KP = 3  # Wzmocnienie regulatora
+TD = 0.005  # Czas wyprzedzenia
 
 timeProbe = 0.1  # Okres czasu próbkowania [s]
 totalTime = 3600  # Czas symulacji [s]
@@ -34,28 +33,30 @@ N = int(totalTime/timeProbe) + 1
 def genOutFlow():
     global outFlow
     outFlow = [0.0 for _ in range(N+1)]
-    for i in range(10000, 11501):
+    for i in range(10000, 10000 + 5*60*10):
+        outFlow[i] = 0.131666
+    for i in range(15000, 15000 + 60*10):
         outFlow[i] = 0.131666
 
 
 def voltage(en):
     if len(volt) < 2:
-        return KP * (en + (timeProbe)/(TI) * sum(e) + (TD)/(timeProbe) * volt[-1])
-    return KP * (en + (timeProbe)/(TI) * sum(e) + (TD)/(timeProbe) * (volt[-1] - volt[-2]))
+        return KP * (en + TD/timeProbe * volt[-1])
+    return KP * (en + TD/timeProbe * (volt[-1] - volt[-2]))
 
 
-# genOutFlow()
+genOutFlow()
 
 for i in range(N):
     time.append(time[-1] + timeProbe)
-    outFlow.append(0.0)
-    inFlow.append(outFlow[-1])
+    # outFlow.append(0.0)
+    inFlow.append(outFlow[i])
     e.append(TEMP_SET - temp[-1])
     volt.append(min(VOLT_MAX, max(VOLT_MIN, voltage(e[-1]))))
     energy.append(ENERGY_MAX * (volt[-1]-VOLT_MIN)/(VOLT_MAX-VOLT_MIN))
     # print(energy[-1])
     heaterPower.append(energy[-1]/timeProbe)
-    temp.append(temp[-1] * ((CAPACITY-outFlow[-1]*timeProbe)/CAPACITY) +
+    temp.append(temp[-1] * ((CAPACITY-outFlow[i]*timeProbe)/CAPACITY) +
                 TEMP_IN * (inFlow[-1]*timeProbe/CAPACITY) +
                 (energy[-1])/(CV*CAPACITY))
     # print(temp[-1]*((CAPACITY-outFlow[-1])/CAPACITY) + TEMP_IN * (inFlow[-1]/CAPACITY))
