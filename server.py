690c1ba5aabea762389eae86de8f4109aa689014
smazik02 <s.mazik2002@gmail.com>
2023-11-21 15:02:08 +0100

    21.11.2023

--- server.py
+++ server.py
@@ -4,49 +4,38 @@ from dash import html
 import plotly.express as px
 import sim
 import pandas as pd
-import asyncio
-import threading
 
-
-def displayGraphs():
-    # stats = sim.getStats()
-    # print(stats)
-
-    heightChart = px.line(x=sim.time,
-                          y=sim.height,
-                          title='Height')
-    df = pd.DataFrame({
-        'time': sim.time,
-        'in': sim.inFlow,
-        'out': sim.outFlow
-    })
-    flowChart = px.line(df, x='time', y=df.columns[1:3], title='Flow')
-    voltChart = px.line(x=sim.time,
-                        y=sim.volt,
-                        title='Voltage')
-
-    return html.Div(children=[
-        html.H1(children='Hello Dash'),
-        html.Div(children='''
-            Dash: A web application framework for Python.
-            '''),
-        dcc.Graph(
-            id='height',
-            figure=heightChart
-        ),
-        dcc.Graph(
-            id='flow',
-            figure=flowChart
-        ),
-        dcc.Graph(
-            id='volt',
-            figure=voltChart
-        )
-    ])
-
-
-simulation = threading.Thread(target=sim.sim, daemon=True)
-simulation.start()
 app = dash.Dash(__name__)
-app.layout = displayGraphs
-app.run_server(debug=True)
+
+tempChart = px.line(x=sim.time,
+                    y=sim.temp,
+                    title='Water temperature')
+voltChart = px.line(x=sim.time,
+                    y=sim.volt,
+                    title='Voltage')
+powerChart = px.line(x=sim.time,
+                     y=sim.heaterPower,
+                     title='Heater power')
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+    html.Div(children='''
+        Dash: A web application framework for Python.
+        '''),
+    dcc.Graph(
+        id='height',
+        figure=tempChart
+    ),
+    dcc.Graph(
+        id='volt',
+        figure=voltChart
+    ),
+    dcc.Graph(
+        id='power',
+        figure=powerChart
+    )
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
--- sim.py
+++ sim.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from time import sleep
 
 CAPACITY = 50  # Pojemność bojlera [litr]
 TEMP_SET = 45  # Ustawiona temperatura wody [*C]
@@ -6,8 +7,9 @@ TEMP_MAX = 80
 TEMP_IN = 15  # Temperatura zimnej wody w kranie
 VOLT_MIN = 0
 VOLT_MAX = 10
-POWER_MIN = 0
-POWER_MAX = 1500
+ENERGY_MIN = 0
+ENERGY_MAX = 1500
+CV = 4180  # ciepło właściwe wody, J * 1/kg * 1/K
 
 KP = 0.025  # Wzmocnienie regulatora
 TI = 2.5  # Czas zdwojenia
@@ -22,9 +24,16 @@ temp = [15.0]  # Temperatura wody
 volt = [0.0]  # Napięcie sterujące
 outFlow = [0.0]  # Dopływ wody
 inFlow = [0.0]  # Odpływ wody
-heaterPower = [0.0]
+energy = [0.0]  # Energia rozgrzewania wody
+heaterPower = [0.0]  # Moc grzałki wody
 
 N = int(totalTime/timeProbe) + 1
+
+
+def genOutFlow():
+    global outFlow
+    outFlow = [0.0 for _ in range(N)]
+    # print(outFlow)
 
 
 def voltage(en):
@@ -33,13 +42,24 @@ def voltage(en):
     return KP * (en + (timeProbe)/(TI) * sum(e) + (TD)/(timeProbe) * (volt[-1] - volt[-2]))
 
 
-def power(un):
-    return ((POWER_MAX-POWER_MIN)*(un-VOLT_MIN))/(VOLT_MAX-VOLT_MIN) + POWER_MIN
+def heatEnergy(un):
+    return ((ENERGY_MAX-ENERGY_MIN)*(un-VOLT_MIN))/(VOLT_MAX-VOLT_MIN) + ENERGY_MIN
 
 
-for _ in range(N):
+genOutFlow()
+
+for i in range(N):
     time.append(time[-1] + timeProbe)
     e.append(TEMP_SET - temp[-1])
     volt.append(min(VOLT_MAX, max(VOLT_MIN, voltage(e[-1]))))
-    inFlow.append(min(POWER_MAX, max(POWER_MIN, power(volt[-1]))))
-    # temp.append(timeProbe)
+    energy.append(min(ENERGY_MAX, max(ENERGY_MIN, heatEnergy(volt[-1]))))
+    # print(energy[-1])
+    heaterPower.append(energy[-1]/timeProbe)
+    inFlow.append(outFlow[i])
+    temp.append(temp[-1]*((1-outFlow[-1])/CAPACITY) +
+                TEMP_IN * (inFlow[-1]/CAPACITY) +
+                (energy[-1]/10)/(CV*CAPACITY))
+    print(temp[-1]*((1-outFlow[-1])/CAPACITY) +
+          TEMP_IN * (inFlow[-1]/CAPACITY))
+    print((heaterPower[-1]/10)/(CV*CAPACITY))
+    sleep(1)
