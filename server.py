cdabd8de40daa0b20ea8eb2f5d60e6c6f3482087
smazik02 <s.mazik2002@gmail.com>
2023-11-22 21:22:09 +0100

    22.11.2023

--- server.py
+++ server.py
@@ -1,55 +1,70 @@
-import dash
-from dash import dcc
-from dash import html
+from flask import Flask, redirect, url_for, render_template, request
 import plotly.express as px
-import sim
+# import sim
 import pandas as pd
 
-app = dash.Dash(__name__)
+# app = dash.Dash(__name__)
+app = Flask(__name__)
 
-tempChart = px.line(x=sim.time,
-                    y=sim.temp,
-                    title='Water temperature')
-voltChart = px.line(x=sim.time,
-                    y=sim.volt,
-                    title='Voltage')
-powerChart = px.line(x=sim.time,
-                     y=sim.heaterPower,
-                     title='Heater power')
-inFlowChart = px.line(x=sim.time,
-                      y=sim.inFlow,
-                      title='InFlow')
-eChart = px.line(x=sim.time,
-                 y=sim.e,
-                 title='Uchyb')
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    html.Div(children='''
-        Dash: A web application framework for Python.
-        '''),
-    dcc.Graph(
-        id='height',
-        figure=tempChart
-    ),
-    dcc.Graph(
-        id='volt',
-        figure=voltChart
-    ),
-    dcc.Graph(
-        id='power',
-        figure=powerChart
-    ),
-    dcc.Graph(
-        id='imflow',
-        figure=inFlowChart
-    ),
-    dcc.Graph(
-        id='uchyb',
-        figure=eChart
-    )
-])
+@app.route("/")
+def home():
+    return render_template("index.html", content="Hello")
+
+
+@app.route("/<name>")
+def user(name):
+    return render_template("index.html", content=name)
+
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
+
+
+# tempChart = px.line(x=sim.time,
+#                     y=sim.temp,
+#                     title='Water temperature')
+# voltChart = px.line(x=sim.time,
+#                     y=sim.volt,
+#                     title='Voltage')
+# powerChart = px.line(x=sim.time,
+#                      y=sim.heaterPower,
+#                      title='Heater power')
+# inFlowChart = px.line(x=sim.time,
+#                       y=sim.inFlow,
+#                       title='InFlow')
+# eChart = px.line(x=sim.time,
+#                  y=sim.e,
+#                  title='Uchyb')
+
+# app.layout = html.Div(children=[
+#     html.H1(children='Hello Dash'),
+#     html.Div(children='''
+#         Dash: A web application framework for Python.
+#         '''),
+#     dcc.Graph(
+#         id='height',
+#         figure=tempChart
+#     ),
+#     dcc.Graph(
+#         id='volt',
+#         figure=voltChart
+#     ),
+#     dcc.Graph(
+#         id='power',
+#         figure=powerChart
+#     ),
+#     dcc.Graph(
+#         id='imflow',
+#         figure=inFlowChart
+#     ),
+#     dcc.Graph(
+#         id='uchyb',
+#         figure=eChart
+#     )
+# ])
